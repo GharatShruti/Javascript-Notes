be1d656f3f7082522b8b4ced845a1989cddeddfc
GharatShruti <shrutigharat62@gmail.com>
2024-02-10 19:39:20 +0530

    msg

---
+++ values.js
@@ -0,0 +1,34 @@
+let x = 3.14;    
+let y = 3; 
+console.log(x)
+console.log(y)
+
+
+// If you add two numbers, the result will be a number:
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(c)
+
+
+// If you add two strings, the result will be a string concatenation:
+let d = "10";
+let e = "20";
+let f = d + e;
+console.log(f)
+
+
+// If you add a number and a string, the result will be a string concatenation:
+let g = 10;
+let h = "20";
+let i = g + h;
+console.log(i)
+
+
+// let x = "10";
+let j = "10";
+let k = 20;
+let l = j + k;
+console.log(l)
+
+
---
+++ variables.js
@@ -0,0 +1,40 @@
+// Variables are Containers for Storing Data
+
+//Using var
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z)
+
+//Using let
+let a = 5;
+let b = 5;
+let c = a + b;
+console.log(c)
+
+
+//Using const
+const d = 5;
+const e = 6;
+const f = d + e;
+console.log(f)
+
+//Mixed
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total)
+
+//Mixed
+let name = "shruti"
+console.log(name)
+ name = 123
+console.log(name)
+console.log(typeof(name))
+
+// const value cannot be changed
+// const name = "shruti"
+// console.log(name)
+//  name = 123
+// console.log(name)
+// console.log(typeof(name))
