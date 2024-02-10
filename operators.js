d5e57bbbc61702955f7a7a5629f1c3fab4c68772
GharatShruti <shrutigharat62@gmail.com>
2024-02-10 20:01:28 +0530

    msg

---
+++ operators.js
@@ -0,0 +1,44 @@
+//Addition Operator
+let a = 5;
+let b = 2;
+let c = a + b;
+console.log(c)
+
+//Subtraction operator
+let d = 5;
+let e = 2;
+let f = d - e;
+console.log(f)
+
+//Multiplication operator
+let g = 5;
+let h = 2;
+let i = g * h;
+console.log(i)
+
+//Arithmetic operator
+let j = 3;
+let k = (100 + 50) * j;
+console.log(k)
+
+// Addition Assignment Operator
+let l = 10;
+l += 5;
+console.log(l)
+
+//Comparison Operators
+let text1 = "a";
+let text2 = "c";
+let result = text1 < text2;
+console.log(result)
+let text3 = "20";
+let text4 = "5";
+let result1 = text3 < text4;
+console.log(result1)
+
+// Type Operators
+let m = "shruti";
+console.log(typeof(m))
+
+//Bitwise Operators
+//Logical Operators
